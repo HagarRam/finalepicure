5ba6ca795999aae8acecd0bff717f7bf27fea902
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-02-21 15:55:20 +0200

    add key in components

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -1,11 +1,8 @@
-import React from "react";
-import { IChef } from "../ChefPage/ChefPage";
+import React from 'react';
+import { IChef } from '../ChefPage/ChefPage';
 
-
-
-
-const ImageChef: React.FC <IChef>=(props:IChef)=>  {
-const {img, name} = props;
+const ImageChef: React.FC<IChef> = (props: IChef) => {
+	const { img, name } = props;
 	return (
 		<div className="chef-card-element">
 			<img
