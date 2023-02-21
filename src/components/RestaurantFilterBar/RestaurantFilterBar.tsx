437a930af50246210544d4c70b32b115cdc9e511
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-02-21 20:29:42 +0200

    didnt-finish-open-now-smart-filter

--- src/components/RestaurantFilterBar/RestaurantFilterBar.tsx
+++ src/components/RestaurantFilterBar/RestaurantFilterBar.tsx
@@ -1,84 +1,119 @@
 import React, { useState } from 'react';
 import './RestaurantFilterBar.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
-  setAllRestuarants,
-  setPopularRestuarants,
-  setNewRestuarants,
-  setOpenNow,
-  setMap,
+	setAllRestuarants,
+	setPopularRestuarants,
+	setNewRestuarants,
+	setOpenNow,
+	setMap,
 } from '../../store/slices/restaurantsSlice';
 import MapContainer from '../MapView/MapContainer';
+import { IRestaurants } from '../RestaurantPage/RestaurantPage';
+import { Rootstate } from '../../store/store';
+import { isRestElement } from '@babel/types';
 
 const RestaurantFilterBar: React.FC = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
+	const filteredRestaurants = useSelector(
+		(state: Rootstate) => state.restaurants.filteredValue
+	);
+	const [isActiveAll, setIsActiveAll] = useState(false);
+	const [isActiveNew, setIsActiveNew] = useState(false);
+	const [isActiveMost, setIsActiveMost] = useState(false);
+	const [isActiveOpen, setIsActiveOpen] = useState(false);
+	const [isActiveMap, setIsActiveMap] = useState(false);
 
-  const [isActiveAll, setIsActiveAll] = useState(false);
-  const [isActiveNew, setIsActiveNew] = useState(false);
-  const [isActiveMost, setIsActiveMost] = useState(false);
-  const [isActiveOpen, setIsActiveOpen] = useState(false);
-  const [isActiveMap, setIsActiveMap] = useState(false);
+	const handleSetActive = (active: string) => {
+		setIsActiveAll(false);
+		setIsActiveNew(false);
+		setIsActiveMost(false);
+		setIsActiveOpen(false);
+		setIsActiveMap(false);
 
-  const handleSetActive = (active: string) => {
-    setIsActiveAll(false);
-    setIsActiveNew(false);
-    setIsActiveMost(false);
-    setIsActiveOpen(false);
-    setIsActiveMap(false);
+		if (active === 'All') {
+			dispatch(setAllRestuarants());
+			setIsActiveAll(true);
+		} else if (active === 'New') {
+			dispatch(setNewRestuarants());
+			setIsActiveNew(true);
+		} else if (active === 'Most') {
+			dispatch(setPopularRestuarants());
+			setIsActiveMost(true);
+		} else if (active === 'Map') {
+			dispatch(setMap());
+			setIsActiveMap(true);
+		} else if (active === 'Open') {
+			dispatch(setOpenNow());
+			setIsActiveOpen(true);
+			getOpenNowRestaurants(filteredRestaurants);
+		}
+	};
+	const getOpenNowRestaurants = (restaurants: IRestaurants[]) => {
+		const date = new Date();
+		const hour = date.getHours();
+		const minutes = date.getMinutes();
+		const currentTime = hour + minutes / 60;
+		const arr: IRestaurants[] = [];
+		restaurants.forEach((restaurant: IRestaurants) => {
+			const openingTime = restaurant.openHours?.[0]
+				? parseFloat(restaurant.openHours?.[0].replace(':', '.'))
+				: undefined;
+			const closingTime = restaurant.openHours?.[1]
+				? parseFloat(restaurant.openHours?.[1].replace(':', '.'))
+				: undefined;
+			if (
+				openingTime &&
+				closingTime &&
+				openingTime <= currentTime &&
+				closingTime >= currentTime
+			) {
+				arr.push(restaurant);
+				console.log(`${restaurant.name} is open`);
+			} else {
+				console.log(`${restaurant.name} is closed`);
+			}
+		});
+		return arr;
+	};
 
-    if (active === 'All') {
-      dispatch(setAllRestuarants());
-      setIsActiveAll(true);
-    } else if (active === 'New') {
-      dispatch(setNewRestuarants());
-      setIsActiveNew(true);
-    } else if (active === 'Most') {
-      dispatch(setPopularRestuarants());
-      setIsActiveMost(true);
-    } else if (active === 'Open') {
-      dispatch(setOpenNow());
-      setIsActiveOpen(true);
-    } else if (active === 'Map') {
-      dispatch(setMap());
-      setIsActiveMap(true);
-    }
-  };
-
-  return (
-    <div id="FilterRestaurant">
-      <button
-        className={isActiveAll ? 'filter-button' : 'without-filter-button'}
-        onClick={() => handleSetActive('All')}
-      >
-        All
-      </button>
-      <button
-        className={isActiveNew ? 'filter-button' : 'without-filter-button'}
-        onClick={() => handleSetActive('New')}
-      >
-        New
-      </button>
-      <button
-        className={isActiveMost ? 'filter-button' : 'without-filter-button'}
-        onClick={() => handleSetActive('Most')}
-      >
-        Most Popular
-      </button>
-      <button
-        className={isActiveOpen ? 'filter-button' : 'without-filter-button'}
-        onClick={() => handleSetActive('Open')}
-      >
-        Open Now
-      </button>
-      <button
-        className={isActiveMap ? 'filter-button' : 'without-filter-button'}
-        onClick={() => handleSetActive('Map')}
-      >
-        Map View
-      </button>
-      {isActiveMap && <div className='Map-Container'><MapContainer lat={0} lng={0} /></div>}
-    </div>
-  );
+	return (
+		<div id="FilterRestaurant">
+			<button
+				className={isActiveAll ? 'filter-button' : 'without-filter-button'}
+				onClick={() => handleSetActive('All')}>
+				All
+			</button>
+			<button
+				className={isActiveNew ? 'filter-button' : 'without-filter-button'}
+				onClick={() => handleSetActive('New')}>
+				New
+			</button>
+			<button
+				className={isActiveMost ? 'filter-button' : 'without-filter-button'}
+				onClick={() => handleSetActive('Most')}>
+				Most Popular
+			</button>
+			<button
+				className={isActiveOpen ? 'filter-button' : 'without-filter-button'}
+				onClick={() => handleSetActive('Open')}>
+				Open Now
+			</button>
+			<button
+				className={isActiveMap ? 'filter-button' : 'without-filter-button'}
+				onClick={() => handleSetActive('Map')}>
+				Map View
+			</button>
+			{isActiveMap && (
+				<div className="Map-Container">
+					<MapContainer
+						lat={0}
+						lng={0}
+					/>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default RestaurantFilterBar;
--- src/store/slices/restaurantsSlice.tsx
+++ src/store/slices/restaurantsSlice.tsx
@@ -1,46 +1,53 @@
-import { createSlice } from "@reduxjs/toolkit";
-import data from '../../data.json'
+import { createSlice } from '@reduxjs/toolkit';
+import { IRestaurants } from '../../components/RestaurantPage/RestaurantPage';
+import data from '../../data.json';
 export const restaurantsSlice = createSlice({
-  name: "restaurants",
-  initialState: {
-    value: data.restaurant,
-    filteredValue: data.restaurant,
-  },
-  reducers: {
-    setAllRestuarants:(state)=>{
-      state.filteredValue=  data.restaurant;
-    },
-    setPopularRestuarants:(state)=>{
-      state.filteredValue= [ ...data.restaurant].sort((a:any, b:any) => b.views - a.views);
+	name: 'restaurants',
+	initialState: {
+		value: data.restaurant,
+		filteredValue: data.restaurant,
+	},
+	reducers: {
+		setAllRestuarants: (state) => {
+			state.filteredValue = data.restaurant;
+		},
+		setPopularRestuarants: (state) => {
+			state.filteredValue = [...data.restaurant].sort(
+				(a: any, b: any) => b.views - a.views
+			);
+		},
+		setNewRestuarants: (state) => {
+			state.filteredValue = data.restaurant.filter(
+				(restaurant) => restaurant.newRest === true
+			);
+		},
+		setOpenNow: (state) => {
+			// state.filteredValue = data.restaurant.filter(
+			// (restaurant) => restaurant.openNow === true
+			// );
+			// state.filteredValue = data.restaurant.filter((restaurant) => {
+			// 	return restaurant.openHours;
+			// });
+			state.filteredValue = data.restaurant;
+		},
+		setMap: (state) => {
+			// state.filteredValue = action.payload.map((restaurant: any) => {
+			//   return {
+			//     locationLat: restaurant.locationLat,
+			//     locationLng: restaurant.locationLng
+			//   };
+			//  });
+			state.filteredValue = data.restaurant;
+		},
+	},
+});
 
-      
-    },
-    setNewRestuarants:(state)=>{
-      state.filteredValue=  data.restaurant.filter(restaurant =>   restaurant.newRest === true
-      );
-    },
-    setOpenNow:(state)=>{
-      state.filteredValue=  data.restaurant.filter(restaurant =>   restaurant.openNow === true);
-    },
-    setMap: (state) => {
-      // state.filteredValue = action.payload.map((restaurant: any) => {
-      //   return {
-      //     locationLat: restaurant.locationLat,
-      //     locationLng: restaurant.locationLng
-      //   };
-      //  });
-      state.filteredValue=  data.restaurant;
-    }
-    },
-     
-    },
-
-
-);
-
-
-
-export const { setAllRestuarants,setOpenNow,setMap, setPopularRestuarants, setNewRestuarants } = restaurantsSlice.actions;
+export const {
+	setAllRestuarants,
+	setOpenNow,
+	setMap,
+	setPopularRestuarants,
+	setNewRestuarants,
+} = restaurantsSlice.actions;
 
 export default restaurantsSlice.reducer;
-
